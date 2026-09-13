905a9065fdf41c59df9c3b2b3ca5b1dbac568f81
AbhishekG-cloud <mritunjaygoswami5@gmail.com>
2026-09-13 17:42:46 +0530

    Time: 329 ms (69.36%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-select score,`rank`
-from (
-    select score,dense_rank() over(order by score desc) as `rank`
-    from Scores
-    
-) t
+select score,
+dense_rank() over(order by score desc ) as 'rank'
+from Scores
+
+
